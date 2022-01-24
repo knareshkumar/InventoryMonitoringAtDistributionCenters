b22bcfa0ee0da3419342e9c30e674d620dcf7031
knareshkumar <nareshkumar2@gmail.com>
2022-01-24 14:08:56 +0530

    Add all project files

--- README.md
+++ README.md
@@ -1,2 +1,187 @@
-# Inventory Monitoring at Distribution Centers
-Udacity - AWS Machine Learning Engineer - Capstone Project
+# Project Overview: Inventory Monitoring at Distribution Centers
+
+This is a Deep learning project done as part of Udacity's capstone project of **AWS Machine Learning Engineer Nanodegree Program**.
+Distribution centers often use robots to move objects as a part of their operations. Objects are carried in bins which can contain multiple objects. Occasionally, items are misplaced while being handled, so the contents of some bin images may not match the recorded inventory of that bin.
+
+The proposed solution is to train and build a Deep Learning model using a pretrained model that can classify the image based on the number of objects in the bin. The input to the model is an image of a bin with the products in it and the output is list of predicted scores for each category type in this case the number of objects in the bin.
+
+Here, I used AWS Sagemaker to train a pretrained model **resnet50** that can perform image classification. 
+
+
+# Overview of the tasks
+
+### AWS SageMaker Setup and installation
+
+To build this project, you wlll have to use AWS through your classroom. Below are your main steps:
+- Open AWS through the classroom on the left panel (**Open AWS Gateway**)
+- Open SageMaker Studio and create a folder for your project
+
+Create and open a Sagemaker instance, I have chosen 'ml.t2.medium' instance type. This instance comes with 2vCPUs and 4GiB memory. This configuration is good enough for running the project jupyter notebook and the price is also one of the least.
+Then create or load the project files required to your workspace.
+
+#### Download the Starter Files
+
+You can clone this Github Repo or download the project files from Github and upload starter files to your workspace.
+
+### Project files
+
+#### Model Training Scripts (train.py / hpo.py / inference.py)
+
+The training script takes the hyperparameters as arguments and reads, loads and preprocesses the train, test and validation datasets. Then after creating and loading the pretrained ‘resnet50’ model it trains the model using the given parameters.
+
+hpo.py:  used for hyperparameter tuning job.
+
+train.py: used for fine tuning the model with best hyperparameters.
+
+inference.py: used for deploying the trained model.
+
+
+#### SageMaker submission script (sagemaker.ipynb)
+
+This jupyter notebook has the code cells to interface with the sagemaker and submit all the jobs to it. The following jobs are done using this script;
+
+- Install necessary dependencies and import the libraries required
+
+- Data download and preparation
+
+- Upload the downloaded data to the required S3 bucket
+
+- Setup the training estimator
+
+- Submit the job
+
+- Setup the Hyperparameter search space
+    
+- Setup the model debugging and profiling rules
+    
+- Create the model estimator for Hyperparameter tuning job
+
+- Create the tuner for Hyperparameter tuning job
+- Submit the tuning job
+    
+- Fetch the best training job and the hyperparameters
+
+- Finetune the model with the best hyperparameters
+    
+- Deploy the model
+    
+- Query the model with a bin image and get prediction
+
+
+
+
+#### Run the submission script (sagemaker.ipynb)
+
+Run the code cells in the jupyter notebook to perform all the steps of the project.
+
+
+
+## Dataset
+
+Initial code cells of sagemaker.ipynb has the steps to data download and process.
+For this project I will be using the <a href="https://registry.opendata.aws/amazon-bin-imagery/" target="_blank">Amazon Bin Image Dataset</a>.
+The dataset contains 500,000 images of bins containing one or more objects. For each image there is a metadata file containing information about the image like the number of objects, it's dimension and the type of object.
+
+Download the subset of the Amazon bin image dataset[1]. 
+
+To build this project you will have to use the [Amazon Bin Images Dataset](https://registry.opendata.aws/amazon-bin-imagery/)
+Since this is a large dataset, I have used the provided code to download a small subset of that data.
+The file_list.json has the list of files to download.
+
+The dataset is split into train, test and validation directories in the ratio of 8:1:1.
+The data that has been uploaded can be verified using the AWS S3 CLI or the S3 UI.
+
+
+# Standout Suggestions
+
+Here are some of the suggestions perfornmed to improve the project:
+
+- Hyperparameter Tuning
+
+- Model Deployment
+
+
+## Hyperparameter Tuning
+Here I used the 'resnet50' pretrained model for this experiment.
+
+The hyperparameters and the ranges used are;
+
+```
+learning-rate: ContinuousParameter(0.001, 0.1)
+batch-size:    CategoricalParameter([32, 64, 128, 256, 512])
+epochs:        IntegerParameter(2,10)
+test-batch-size: 100
+```
+
+**Screenshot of Hyperparameter tuning job**
+![Screenshot Hyperparameter tuning job](./screenshots/hpo.png)
+    
+### The best hyperparameters from all the training jobs are
+
+```
+  'test-batch-size': 100,
+  'epochs': 8,
+  'batch-size': 256,
+  'lr': 0.003820049499119736
+```
+
+## Debugging and Profiling
+
+__Summary from logs:__
+
+```
+modelVanishingGradient: NoIssuesFound
+
+Overfit: NoIssuesFound
+
+Overtraining: NoIssuesFound
+
+PoorWeightInitialization: NoIssuesFound
+
+LossNotDecreasing: NoIssuesFound
+
+LowGPUUtilization: IssuesFound
+
+ProfilerReport: IssuesFound
+```
+
+
+## Model Deployment
+
+The model has been deployed using the PyTorchModel. For this I used a separate script 'inference.py'.
+
+The endpoint can be queried using the code in the notebook. The test image needs to downloaded to the workspace directory and use the file location to fetch the image. Then preprocess the image and pass it to endpoint for prediction.
+
+**Screenshot of the endpoint SageMaker**
+![Screenshot Endpoint](./screenshots/endpoint.png)
+
+
+## Querying the model endpoint
+
+The endpoint can be queried using the predict() method of the predictor that was deployed.
+Test image needs to be passed as below;
+
+
+```
+from PIL import Image
+import io
+
+def image_to_byte_array(image:Image):
+  imgByteArr = io.BytesIO()
+  image.save(imgByteArr, format=image.format)
+  imgByteArr = imgByteArr.getvalue()
+  return imgByteArr
+
+img = Image.open("TestImgs/00010.jpg", mode='r')
+img_bytes = image_to_byte_array(img)
+type(img_bytes)
+
+
+response = predictor.predict(img_bytes, initial_args={"ContentType": "image/jpeg"})
+```
+
+## Results
+
+**Screenshot of the results**
+
+![Screenshot Results](./screenshots/result.png)
---
+++ hpo.py
@@ -0,0 +1,213 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import argparse
+import logging
+import sys
+import os
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def test(model, test_loader, criterion, device):
+    '''
+    Complete this function that can take a model and a 
+          testing data loader and will get the test accuray/loss of the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    print("Testing Model on Whole Testing Dataset")
+    model.eval()
+    running_loss=0
+    running_corrects=0
+    
+    for inputs, labels in test_loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+    total_loss = running_loss / len(test_loader.dataset)
+    total_acc = running_corrects/ len(test_loader.dataset)
+    
+    logger.info(f"Test set: Average loss: {total_loss}")
+    logger.info(f"Testing Accuracy: {total_acc}")
+
+
+def train(model, train_loader, validation_loader, criterion, optimizer, device, epochs):
+    '''
+    Complete this function that can take a model and
+          data loaders for training and will get train the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    logger.info("Start Model Training")
+
+    best_loss = 1e6
+    image_dataset = {'train':train_loader, 'valid':validation_loader}
+    loss_counter = 0
+
+    for epoch in range(epochs):
+        for phase in ['train', 'valid']:
+            print(f"Epoch {epoch}, Phase {phase}")
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for step, (inputs, labels) in enumerate(image_dataset[phase]):
+                #inputs = torch.transpose(inputs)
+                #inputs = torch.unsqueeze(inputs, 1)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(image_dataset[phase])
+            epoch_acc = running_corrects / len(image_dataset[phase])
+            
+            if phase == 'valid':
+                if epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                else:
+                    loss_counter += 1
+            logger.info('{} loss: {:.4f}, acc: {:.4f}, best loss: {:.4f}'.format(phase, epoch_loss, epoch_acc, best_loss))
+
+        if loss_counter == 1:
+            break
+    return model
+
+
+def net():
+
+    #initializes a pretrained model
+    
+    model = models.resnet50(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features=model.fc.in_features
+
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 64),
+                   nn.ReLU(inplace=True),
+                   nn.Linear(64, 5))
+    return model 
+
+
+def create_data_loaders(train_data, test_data, validation_data, batch_size):
+    #data loaders for train, test and validation data sets
+
+    training_transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+
+    testing_transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+  
+    train_set = torchvision.datasets.ImageFolder(root = train_data, transform = training_transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size = batch_size)
+    
+    test_set = torchvision.datasets.ImageFolder(root = test_data, transform = testing_transform)
+    test_loader  = torch.utils.data.DataLoader(test_set, batch_size = batch_size)
+    
+    validation_set = torchvision.datasets.ImageFolder(root = validation_data, transform = testing_transform)
+    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size = batch_size) 
+    
+    return train_loader, test_loader, validation_loader
+
+
+def main(args):
+    #Initialize a model by calling the net function
+    model = net()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    print(f"Running on Device {device}")
+    
+    #Create your loss and optimizer
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr = args.lr)
+    
+    train_loader, test_loader, validation_loader = create_data_loaders(
+                        args.train_data, args.test_data, args.val_data, args.batch_size)
+    
+    '''
+    TODO: Call the train function to start training your model
+    Remember that you will need to set up a way to get training data from S3
+    '''
+    model = train(model, train_loader, validation_loader, loss_criterion, optimizer, device, args.epochs)
+    
+    
+    '''
+    TODO: Test the model to see its accuracy
+    '''
+    logger.info("Start Model Testing")
+    test(model, test_loader, loss_criterion, device)
+    
+    '''
+    TODO: Save the trained model
+    '''
+    logger.info("Model Saving")
+    torch.save(model.state_dict(), os.path.join(args.model_dir, "model.pth"))
+
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    # Specify all the hyperparameters you need to use to train your model.
+    # Training settings
+    parser.add_argument(
+        "--batch-size", type=int, default=64
+    )
+    parser.add_argument(
+        "--test-batch-size", type=int, default=32
+    )
+    parser.add_argument(
+        "--epochs", type=int, default=10
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0
+    )
+    parser.add_argument(
+        "--model-dir", type=str, default=os.environ["SM_MODEL_DIR"]
+    )
+    parser.add_argument(
+        "--train_data", type=str, default=os.environ["SM_CHANNEL_TRAIN"]
+    )
+    parser.add_argument(
+        "--test_data", type=str, default=os.environ["SM_CHANNEL_TEST"]
+    )
+    parser.add_argument(
+        "--val_data", type=str, default=os.environ["SM_CHANNEL_VAL"]
+    )
+    
+    args = parser.parse_args()
+    print("args to hpo.py : ", args)
+    main(args)
